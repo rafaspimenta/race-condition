df00cfc16286ebea2a18db3121a9d0f6f79845dd
Rafael Pimenta <rafael.pimenta@hexagon.com>
2023-11-07 18:29:35 -0300

    study race condition

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RaceCondition
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var semaphore = new SemaphoreSlim(1);
+
+            var tasks = new List<Task>();
+            var resource = 0;
+
+            async Task work()
+            {
+                await semaphore.WaitAsync();
+                resource++;
+                semaphore.Release();
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var t = work();
+                tasks.Add(t);
+
+            }
+
+            await Task.WhenAll(tasks);
+            Console.WriteLine(resource);
+        }
+    }
+}
